515b8e5ffe19911ecbac1bd775629608f3182a3e
ricardoOpus <ricardoopus125@gmail.com>
2023-04-14 05:30:40 -0300

    Detail timeline

--- components/detailTimeline/script.js
+++ components/detailTimeline/script.js
@@ -1,75 +1,77 @@
-changeTabColor("timeline");
+/* eslint-disable no-undef */
+changeTabColor('timeline');
 
 async function restoreCharactersCard() {
   const currentCardID = await getCurrentCardID();
   const projectData = await getCurrentProject();
-  projectData.data.timeline.forEach( (ele) => {
+  projectData.data.timeline.forEach((ele) => {
     if (ele.id === currentCardID) {
-      Object.keys(ele).forEach(key => {
+      Object.keys(ele).forEach((key) => {
         const result = document.getElementById(key);
         if (result) {
-          return result.value = ele[key];
+          result.value = ele[key];
+          return result.value;
         }
-      })
-      resumeHeight("content")
-    } else {
-      return null
-    }
-  })
-};
-
-var elementsArray = document.querySelectorAll(".projectInputForm");
-
-elementsArray.forEach(async function(elem) {
-  const currentID = await getCurrentProjectID();
-  const currentCardID = await getCurrentCardID();
-  const projectData = await getCurrentProject();
-  const positionInArray = await getCurrentCard();
-  projectData.data.timeline.forEach( (ele) => {
-    if (ele.id === currentCardID) {
-      elem.addEventListener("input", async () => {
-        await lastEditListModify('timeline', currentCardID);
-        const field = elem.id
-        await db.projects.where('id').equals(currentID).modify( (e) => {
-          e.data.timeline[positionInArray][field] = elem.value;
-        });
-        updateLastEdit(currentID);
+        return null;
       });
-    } else {
-      return null
+      resumeHeight('content');
     }
-  })
-});
+  });
+}
 
-$( "#dialog-delete-timeline" ).dialog({
-	autoOpen: false,
-	width: 500,
-	buttons: [
-		{
-			text: "Ok",
-			click: async function() {
+function saveValues() {
+  const elementsArray = document.querySelectorAll('.projectInputForm');
+  elementsArray.forEach(async (elem) => {
+    const currentID = await getCurrentProjectID();
+    const currentCardID = await getCurrentCardID();
+    const projectData = await getCurrentProject();
+    const positionInArray = await getCurrentCard();
+    projectData.data.timeline.forEach((ele) => {
+      if (ele.id === currentCardID) {
+        elem.addEventListener('input', async () => {
+          await lastEditListModify('timeline', currentCardID);
+          const field = elem.id;
+          await db.projects.where('id').equals(currentID).modify((e) => {
+            e.data.timeline[positionInArray][field] = elem.value;
+          });
+          updateLastEdit(currentID);
+        });
+      }
+      return null;
+    });
+  });
+}
+saveValues();
+
+$('#dialog-delete-timeline').dialog({
+  autoOpen: false,
+  width: 500,
+  buttons: [
+    {
+      text: 'Ok',
+      async click() {
         await deleteCard('timeline');
-        $( this ).dialog( "close" );
+        $(this).dialog('close');
         loadpage('timeline');
-			}
-		},
-		{
-			text: "Cancel",
-      id: "btnTwo",
-			click: function() {
-				$( this ).dialog( "close" );
-			}
-		}
-	]
+      },
+    },
+    {
+      text: 'Cancel',
+      id: 'btnTwo',
+      click() {
+        $(this).dialog('close');
+      },
+    },
+  ],
 });
 // Link to open the dialog
-$( "#deleteTimelineCard" ).click(function( event ) {
-	$( "#dialog-delete-timeline" ).dialog( "open" );
-  $("#btnTwo").focus();
-	event.preventDefault();
+$('#deleteTimelineCard').click((event) => {
+  $('#dialog-delete-timeline').dialog('open');
+  $('#btnTwo').focus();
+  event.preventDefault();
 });
 
 restoreCharactersCard();
 // restoreCategories('world');
 
-restorePOV("#pov_id", "characters");
+restorePOV('#pov_id', 'characters');
