87edcb75a527817051995bdaf0d0a46bcef8bbda
ricardoOpus <ricardoopus125@gmail.com>
2023-06-29 21:54:04 -0300

    Refactor: function go to top read in dashboard

--- pages/cenas/script.js
+++ pages/cenas/script.js
@@ -237,16 +237,3 @@ $(() => {
 function clearStatusFilter() {
   loadpage('cenas');
 }
-
-function topFunction() {
-  document.body.scrollTop = 0; // Para navegadores Safari
-  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
-}
-
-window.onscroll = function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById('topButton').style.display = 'block';
-  } else {
-    document.getElementById('topButton').style.display = 'none';
-  }
-};
